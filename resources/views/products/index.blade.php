84642d606e309f6077f1d31a7d7bba5c2b5ee3e5
Neyser Gómez <dneyser.rockergomez@gmail.com>
2018-08-08 11:38:22 -0500

    Vista. Listar y paginar productos

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layout')
+
+@section('content')
+    <div class="col-sm-8">
+        <h2>
+            Listado de productos
+            <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">Nuevo</a>
+        </h2>
+        <table>
+            <thead>
+                <tr>
+                    <th width="20px">ID</th>
+                    <th>Nombre del producto</th>
+                    <th colspan="3">&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>
+                        <strong>{{ $product->name }}</strong>
+                        {{ $product->short }}
+                    </td>
+                    <td>Ver</td>
+                    <td>Editar</td>
+                    <td>Borrar</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {!! $products->render() !!}
+    </div>
+    <div class="col-sm-4">
+        Mensaje
+    </div>
+@endsection
